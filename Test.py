dbcc4ebc343c265edf43cad860baff758279527c
Helocoding <dias.2031@isbergen.no>
2026-01-18 13:22:57 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,39 @@
+#This is a test file for python
+print("Hello world:)")
+print("45")
+print("45")
+print("the marks you have got", 4)
+print(3+78)
+print("hello there", end = " ")
+print("welcome")
+
+age = 12
+name1 = "Dia"
+d = 33.33
+place = input("Please enter the city you live in :")
+
+
+print(place)
+
+dog = int(input("how many pets do you have? : "))
+
+
+import keyword as k
+print(k.kwlist) 
+
+num1 = int(input("type any number :"))
+num2 = int(input("type any number :"))
+
+print(num1 + num2)
+
+name = '123'
+height = 6.3
+age = 12 
+isavailable = False
+
+print("The name of my friend is", name)
+
+print(type(name))
+print(type(place))
+print(type(age))
+print(type(isavailable))
